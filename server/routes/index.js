a2a845069db0a8cb853c8bb8640e707d30300a6c
clong desktop <chip_long@yahoo.com>
2022-06-05 17:41:23 -0600

    added post test route

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,6 +5,17 @@ const apiRoutes = require("./api");
 
 // setup routes
 router.use("/api", apiRoutes);
+router.post("/post", (req, res, next) => {
+  console.log(req.body);
+  try {
+    res.statusCode = 200;
+    res.json({
+      data: `post test done on ${req.body.date} at ${req.body.time}`,
+    });
+  } catch (err) {
+    return next(err);
+  }
+});
 router.get("/", (req, res) => {
   console.log("network test request");
   res.send({ success: "success" });
